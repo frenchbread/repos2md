a381373f734c53193b92b016621cea977b1d84d8
Damir Mustafin <frenchthebread@gmail.com>
2016-04-10 23:50:56 +0300

    Move func to module exports

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const getRepos = require('./lib/getRepos');
 const parseReposToMd = require('./lib/parseReposToMd');
 const saveToFile = require('./lib/saveToFile');
 
-let username = 'frenchbread';
-let path = __dirname + '/repos.md';
+module.exports = (username, path) => {
 
-getRepos(username, (err, repos) => {
+  getRepos(username, (err, repos) => {
 
-  let heading = `# Repos by @${username}\nAuto-generated list of repositories using [repos2md](https://github.com/frenchbread/repos2md).\n\n`;
+    let heading = `# Repos by @${username}\nAuto-generated list of repositories using [repos2md](https://github.com/frenchbread/repos2md).\n\n`;
 
-  let body = parseReposToMd(repos);
+    let body = parseReposToMd(repos);
 
-  let footer = `> <> with <3`;
+    let footer = `> <> with <3`;
 
-  saveToFile(path, heading+body+footer, (err) => {
+    saveToFile(path, heading+body+footer, (err, path) => {
 
-    if (err) console.log(err);
+      if (err) console.log(err);
 
-    console.log('File saved!');
+      console.log(`File saved to ${path}`);
+    });
   });
-});
+};
