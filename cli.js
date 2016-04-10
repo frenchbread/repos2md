87bfacd235d022972d9d2ed647f48c0955b3b8b0
Damir Mustafin <frenchthebread@gmail.com>
2016-04-10 23:51:16 +0300

    Cli init

---
+++ cli.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const meow = require('meow');
+const repos2md = require('./index');
+
+const cli = meow(`
+  Usage
+    $ repos2md <username>
+    $ repos2md <username> --path=<path>
+
+  Options
+    --path    Absolute path for .md document to be saved to
+
+  Examples
+    $ repos2md frenchbread
+    $ repos2md frenchbread --path=/Users/frenchbread/Desktop
+`);
+
+const input = cli.input;
+
+if (input.length === 0 && process.stdin.isTTY) {
+	console.error('Input required');
+	process.exit(1);
+}
+
+if (input.length) {
+  let username = input[0];
+  let path = (cli.flags.path) ? cli.flags.path : __dirname;
+  path += `/repos-${username}.md`;
+  repos2md(username, path);
+}
