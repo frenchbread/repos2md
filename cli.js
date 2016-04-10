47a894b8ad6cab54cda3acf13880ff327556552e
Damir Mustafin <frenchthebread@gmail.com>
2016-04-11 00:08:38 +0300

    Fix cli.js

--- cli.js
+++ cli.js
@@ -6,27 +6,24 @@ const repos2md = require('./index');
 
 const cli = meow(`
   Usage
-    $ repos2md <username>
-    $ repos2md <username> --path=<path>
+    $ repos2md --username=<username>
+    $ repos2md --username=<username> --path=<path>
 
   Options
-    --path    Absolute path for .md document to be saved to
+    --username  GitHub username (required)
+    --path      Absolute path for .md document to be saved to
 
   Examples
-    $ repos2md frenchbread
-    $ repos2md frenchbread --path=/Users/frenchbread/Desktop
+    $ repos2md --username=frenchbread
+    $ repos2md --username=frenchbread --path=/Users/frenchbread/Desktop
 `);
 
-const input = cli.input;
-
-if (input.length === 0 && process.stdin.isTTY) {
-	console.error('Input required');
-	process.exit(1);
-}
-
-if (input.length) {
-  let username = input[0];
+if (cli.flags.username) {
+  let username = cli.flags.username;
   let path = (cli.flags.path) ? cli.flags.path : __dirname;
   path += `/repos-${username}.md`;
   repos2md(username, path);
+} else {
+  console.log('Username was not provided!\nUse --help for reference.');
+  process.exit(1);
 }
