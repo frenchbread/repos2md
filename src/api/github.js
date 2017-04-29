171b08661543212599a0483097b8c21420abdd5d
Damir Mustafin <frenchthebread@gmail.com>
2017-04-29 22:55:42 +0300

    Refactor & add `page` query parameter

--- src/api/github.js
+++ src/api/github.js
@@ -4,24 +4,29 @@ import axe from './axe'
 
 export default {
   getUserRepos ({ username, token, page }) {
-    const params = (token) ? { auth: { username, password: token } } : {}
+    let params = {}
+    let url = ''
 
     if (token) {
-      let url = `/user/repos`
+      url = `/user/repos`
       url += '?per_page=100'
+      url += `&page=${page}`
       url += '&sort=updated'
       url += '&affiliation=owner'
-      return axe.get(url, params)
-        .then(res => res.data)
-        .catch(err => l.error(err))
+
+      params = { auth: { username, password: token } }
     } else {
-      let url = `/users/${username}/repos`
+      url = `/users/${username}/repos`
       url += '?per_page=100'
+      url += `&page=${page}`
       url += '&sort=updated'
-      return axe.get(url)
-        .then(res => res.data)
-        .catch(err => l.error(err))
     }
+
+    return axe.get(url, params)
+      .then(res => {
+        return res.data
+      })
+      .catch(err => l.error(err))
   },
   getStarredRepos ({ username, page }) {
     let url = `/users/${username}/starred`
